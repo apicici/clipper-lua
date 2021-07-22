0afc56cf21a47b07fa6803606d0569b9c65dd121
apicici <apicici@apicici.com>
2021-07-22 14:53:52 -0400

    fixed missing compilation of x86 dll

--- .appveyor/build.bat
+++ .appveyor/build.bat
@@ -1,7 +1,6 @@
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32 || set OS=64
-if %OS%==64 (set ARCH=x64) else (set ARCH=Win32)
+if %WIN_ARCH%==64 (set ARCH=x64) else (set ARCH=Win32)
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars%OS%.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars%WIN_ARCH%.bat"
 pip install hererocks
 hererocks lua -l 5.1 --cflags="-fPIC"
 set CMAKE_PREFIX_PATH=%cd%\lua
